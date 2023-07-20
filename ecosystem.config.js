8f5e70b4175de92ad78489b2a338cb8966be2f47
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2023-07-20 12:18:54 -0400

    test

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps : [{
+      name: "app",
+      script: "./server/server.js",
+      env: {
+        NODE_ENV: "production",
+      }
+    }, {
+       name: 'worker',
+       script: 'worker.js'
+    }]
+  }
